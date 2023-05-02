8ff79e0efa42a7e6de3e15b2913c02f47301f40a
tuanhoangle <tuanleprofessional@gmail.com>
2023-05-01 21:03:00 -0400

    Store, Get by ID, Get

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Data;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[controller] automatically look at VillaAPIController and replace it with VillaAPI
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+        }
+    }
+}
